86168f48c8c470e4f210b0dc51c85f11a5f8c895
draconculis <draconculis@hotmail.com>
2019-05-24 16:19:26 +0200

    Adding models

---
+++ tTracker/DBModels/Project.cs
@@ -0,0 +1,6 @@
+public class Project
+{
+    public Guid ProjectId {get;set;}
+    public string Name {get; set;}
+    public string Uri {get;set;}
+}
---
+++ tTracker/DBModels/TimeStamp.cs
@@ -0,0 +1,6 @@
+public class TimeStamp
+{
+    public DateTime Time {get; set;}
+    public Guid Projectd{get;set;}
+    public string Notes {get;set;}
+}
